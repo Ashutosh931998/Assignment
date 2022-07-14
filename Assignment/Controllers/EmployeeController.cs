ce7b7a23469bfb7454882445a67143e7572f967e
Ashutosh931998 <108882339+Ashutosh931998@users.noreply.github.com>
2022-07-14 18:52:10 +0530

    Initial commit

---
+++ Assignment/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Assignment.Models;
+namespace Assignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public static List<EmployeeValidation> lstEmployeevalidation = new List<EmployeeValidation>();
+       public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeValidation st)
+       {
+           lstEmployeevalidation.Add(st);
+           if (!ModelState.IsValid)
+           {
+               return View();
+           }
+           else
+           {
+               return RedirectToAction("Index");
+           }
+       }
+        public ActionResult Index()
+        {
+            return View(lstEmployeevalidation);
+        }
+        public ActionResult Edit(int id)
+        {
+            EmployeeValidation st = lstEmployeevalidation.FirstOrDefault(e => e.Name.Equals(id));
+            return View(st);
+        }
+       
+	}
+}
---
+++ Assignment/Models/EmployeeValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Assignment.Models
+{
+    public class EmployeeValidation
+    {
+        [Required(ErrorMessage = "enter Name")]
+        public string Name { get; set; }
+        [Range(18, 80, ErrorMessage = "Age is not less than 18")]
+        public string Age { get; set; }
+        [Required(ErrorMessage = "enter Salary")]
+        public string salary { get; set; }
+
+
+        public EmployeeValidation EmployeeValidations { get; set; }
+    }
+}
